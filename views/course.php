103fd7d3798756a5e614e2feedbf20cb2784bab0
datntph33960 <datntph33960@fpt.edu.vn>
2025-03-15 12:28:16 +0700

    Add files via upload

---
+++ views/course.php
@@ -0,0 +1,68 @@
+<div class="courses-area section-padding40 fix" style="background-color: #fefefe;">
+    <div class="container">
+        <div class="row justify-content-center my-5">
+            <div class="col-xl-7 col-lg-8">
+                <div class="section-tittle text-center">
+                    <h3>Mua quần thể thao ngay để nhận voucher miễn phí</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="properties properties2 mb-30">
+                    <div class="properties__card">
+                        <div class="properties__img overlay1 text-center">
+                            <a href="#"><img style="max-width: 200px;" src="public/img/courses/quangao1.jpg" alt=""></a>
+                        </div>
+                        <div class="properties__caption">
+                            <p>Quần thể thao</p>
+                            <h3><a href="#">Chương trình hướng dẫn thể thao</a></h3>
+                            <p style="height: 44px;" class="text-truncate-2">Chương trình này dành cho những người mới bắt đầu tập thể thao hoặc muốn nâng cao kỹ năng.</p>
+                            <p class="text-danger">Bạn đã sở hữu chương trình</p>
+                            <a href="#" class="btn btn-dark w-100">Học ngay</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="properties properties2 mb-30">
+                    <div class="properties__card">
+                        <div class="properties__img overlay1 text-center">
+                            <a href="#"><img style="max-width: 200px;" src="public/img/courses/quangao2.jpg" alt=""></a>
+                        </div>
+                        <div class="properties__caption">
+                            <p>Quần thể thao</p>
+                            <h3><a href="#">Voucher mua đồ thể thao</a></h3>
+                            <p style="height: 44px;" class="text-truncate-2">Chương trình này dành cho những người mới bắt đầu tập thể thao hoặc muốn nâng cao kỹ năng.</p>
+                            <p class="text-dark">30 sản phẩm (Mua quần thể thao để nhận ngay chương trình hướng dẫn miễn phí)</p>
+                            <a href="#" class="btn btn-primary w-100">Mua quần thể thao ngay</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="properties properties2 mb-30">
+                    <div class="properties__card">
+                        <div class="properties__img overlay1 text-center">
+                            <a href="#"><img style="max-width: 200px;" src="public/img/courses/quangao3.jpg" alt=""></a>
+                        </div>
+                        <div class="properties__caption">
+                            <p>Quần thể thao</p>
+                            <h3><a href="#">Chương trình hướng dẫn thể thao</a></h3>
+                            <p style="height: 44px;" class="text-truncate-2">Chương trình này dành cho những người mới bắt đầu tập thể thao hoặc muốn nâng cao kỹ năng.</p>
+                            <p class="text-dark">30 sản phẩm (Mua quần thể thao để nhận ngay chương trình hướng dẫn miễn phí)</p>
+                            <a href="#" class="btn btn-primary w-100">Mua quần thể thao ngay</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-8">
+                <div class="section-tittle text-center mt-40">
+                    <a href="#" class="border-btn">Xem thêm</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
