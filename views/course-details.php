f6cf69b36e4c6218d192a362dde9c393be57ce49
datntph33960 <datntph33960@fpt.edu.vn>
2025-03-21 20:27:37 +0700

    Create course-details.php

---
+++ views/course-details.php
@@ -0,0 +1,124 @@
+<style>
+        .video-courses .box-video{
+            width: 100%; height: 600px
+        }
+
+        .video-courses .box-video iframe{
+            width: 100%; height: 100%;
+        }
+
+        .video-courses .box-video video{
+            width: 100%; height: 100%;
+        }
+    </style>
+    <section class="container my-3">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card video-courses">
+                    <div class="card-body">
+                        <a class="card-title-desc text-dark" href="">Quần  thể thao > </a>
+                        <a class="card-title-desc text-dark" href="">Quần thể thao thời trang > </a>
+                        
+                        <hr>
+                     
+                        
+
+               
+                        <div class="flex-center mt-4 mb-3">
+                            <a class="btn btn-outline-default mr-4" href="">PREV</a>
+                            <a class="btn btn-outline-default" href="">NEXT</a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+    </section>
+
+    
+  
+    
+
+    
+    <div class="courses-area section-padding40 fix" style="background-color: #fefefe;">
+        <div class="container">
+            <div class="row justify-content-center my-5">
+                <div class="col-xl-7 col-lg-8">
+                    <div class="section-tittle text-center">
+                        <h3>Chương trình của chúng tôi</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                
+                <div class="col-lg-4">
+                    <div class="properties properties2 mb-30">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1 text-center">
+                                <a href="#"><img style="max-width: 200px;" src="public/img/courses/tieng-anh-nguoi-moi.jpg" alt=""></a>
+                            </div>
+                            <div class="properties__caption">
+                                <p>Tiếng Anh</p>
+                                <h3><a href="#">Khóa học tiếng Anh</a></h3>
+                                <p style="height: 44px;" class="text-truncate-2">Voucher mua đồ miễn phí cho ai nhanh tay
+                                </p>
+                                <p class="text-dark">30 sản phẩm (Mua đồ để nhận ngay voucher miễn phí)
+                                </p>
+                                
+                                <a href="#" class="btn btn-primary w-100">Mua đồ ngay</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="properties properties2 mb-30">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1 text-center">
+                                <a href="#"><img style="max-width: 200px;" src="public/img/courses/tieng-anh-nguoi-moi.jpg" alt=""></a>
+                            </div>
+                            <div class="properties__caption">
+                                <p>Tiếng Anh</p>
+                                <h3><a href="#">Khóa học tiếng Anh</a></h3>
+                                <p style="height: 44px;" class="text-truncate-2">Voucher mua đồ miễn phí cho ai nhanh tay
+                                </p>
+                                <p class="text-dark">30 sản phẩm (Mua đồ để nhận ngay voucher miễn phí)
+                                </p>
+                                
+                                <a href="#" class="btn btn-primary w-100">Mua đồ ngay</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="properties properties2 mb-30">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1 text-center">
+                                <a href="#"><img style="max-width: 200px;" src="public/img/courses/tieng-anh-nguoi-moi.jpg" alt=""></a>
+                            </div>
+                            <div class="properties__caption">
+                                <p>Tiếng Anh</p>
+                                <h3><a href="#">Khóa học tiếng Anh</a></h3>
+                                <p style="height: 44px;" class="text-truncate-2">Voucher mua đồ miễn phí cho ai nhanh tay
+                                </p>
+                                <p class="text-dark">30 sản phẩm (Mua đồ để nhận ngay voucher miễn phí)
+                                </p>
+                                
+                                <a href="#" class="btn btn-primary w-100">Mua đồ ngay</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                
+                
+                
+                
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-xl-7 col-lg-8">
+                    <div class="section-tittle text-center mt-40">
+                        <a href="#" class="border-btn">Xem thêm</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
